abd588305b492cfb2bf9f21cec6e166d592467a6
Graycesun <graypeely@gmail.com>
2026-06-15 14:56:18 -0700

    Add new values to configuartion file

--- js/config.js
+++ js/config.js
@@ -18,8 +18,18 @@ export const CONFIG = {
     SCALE : 3, //sprites are scaled up by this amount. ex, an original size of 16 by scale of 3 is 48 pixesl
     VIEW_TILES_X : 16, //number of tiles seen by the camera in the x direction
     VIEW_TILES_Y : 12, //number of tiles seen by the camera in the y direction
+
     PLAYER_SPEED : 190, //pixels moved per second
     PLAYER_FRAME_SIZE : 48, //Pixel size of player character
+    PLAYER_MAX_HP : 20,
+    PLAYER_ATTACK_DAMAGE : 5,
+    PLAYER_ATTACK_RANGE : 24,
+
+    XP_BASE: 12,
+    XP_GROWTH: 1.5,
+    HP_PER_LEVEL :6,
+    DAMAGE_PER_LEVEL : 2,
+    HEAL_ITEM_AMOUNT : 8,
 
     SOLID_TOP_INSET : 16, //remove unneeded pixels of solid tiles
     ANIM_FPS : 8,
